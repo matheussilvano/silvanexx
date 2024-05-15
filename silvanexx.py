db2aaddb0c944224d71f0b380690a98f78543cdc
Matheus Silvano Pereira <115375444+matheussilvano@users.noreply.github.com>
2024-05-15 16:24:55 -0300

    Create silvanexx.py

---
+++ silvanexx.py
@@ -0,0 +1,20 @@
+import os
+from time import sleep
+
+print('''
+.dP"Y8 88 88     Yb    dP    db    88b 88 888888 Yb  dP Yb  dP 
+`Ybo." 88 88      Yb  dP    dPYb   88Yb88 88__    YbdP   YbdP  
+o.`Y8b 88 88  .o   YbdP    dP__Yb  88 Y88 88""    dPYb   dPYb  
+8bodP' 88 88ood8    YP    dP""""Yb 88  Y8 888888 dP  Yb dP  Yb''')
+sleep(1)
+print('Criado por: Matheus Silvano')
+sleep(1)
+os.system('cls')
+
+print('''===== INICIO =====
+[1] Gerador de scripts manager
+[2] Gerador de comandos SSH
+[3] Gerador de e-mail
+''')
+opcao = input('Digite a opcao desejada: ')
+os.system('cls')
